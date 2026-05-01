7f2c21f9684dff9086da8576b2092fa3d0187e3d
Juwoojae <juwqq1234@naver.com>
2026-05-02 02:09:37 +0900

    [feat] 회원가입, 로그인 api 설계 - memberService  구현 - memberController 구현 - JwtTokenFilter 구현 - JwtTokenProvider 구현

--- backend/src/main/java/com/example/backend/member/controller/MemberController.java
+++ backend/src/main/java/com/example/backend/member/controller/MemberController.java
@@ -1,14 +1,50 @@
 package com.example.backend.member.controller;
 
+import com.example.backend.auth.JwtTokenProvider;
+import com.example.backend.member.domain.Member;
+import com.example.backend.member.dto.MemberCreateDto;
+import com.example.backend.member.dto.MemberLoginDto;
 import com.example.backend.member.service.MemberService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
+@RequestMapping("/member")
 public class MemberController {
 
     private MemberService memberService;
+    private final JwtTokenProvider jwtTokenProvider;
 
-    public MemberController(MemberService memberService) {
+    public MemberController(MemberService memberService, JwtTokenProvider jwtTokenProvider) {
         this.memberService = memberService;
+        this.jwtTokenProvider = jwtTokenProvider;
     }
+
+    @PostMapping("/create")
+    public ResponseEntity<?> memberCreate(@RequestBody MemberCreateDto memberCreateDto){
+        Member member = memberService.create(memberCreateDto);
+        return new ResponseEntity<>(member.getId(), HttpStatus.CREATED);
+    }
+
+    @PostMapping("/doLogin")
+    public ResponseEntity<?> doLogin(@RequestBody MemberLoginDto memberLoginDto){
+//        email, password 일치한지 검증
+        Member member = memberService.login(memberLoginDto);
+
+//        일치할 경우 jwt accesstoken 생성
+        String jwtToken = jwtTokenProvider.createToken(member.getEmail(), member.getRole().toString());
+
+        Map<String, Object> loginInfo = new HashMap<>();
+        loginInfo.put("id", member.getId());
+        loginInfo.put("token", jwtToken);
+        return new ResponseEntity<>(loginInfo, HttpStatus.OK);
+    }
+
 }
--- backend/src/main/java/com/example/backend/member/service/MemberService.java
+++ backend/src/main/java/com/example/backend/member/service/MemberService.java
@@ -1,16 +1,45 @@
 package com.example.backend.member.service;
 
+import com.example.backend.member.domain.Member;
+import com.example.backend.member.dto.MemberCreateDto;
+import com.example.backend.member.dto.MemberLoginDto;
 import com.example.backend.member.repository.MemberRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @Transactional
 public class MemberService {
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    private MemberRepository memberRepository;
-
-    public MemberService(MemberRepository memberRepository) {
+    public MemberService(MemberRepository memberRepository, PasswordEncoder passwordEncoder) {
         this.memberRepository = memberRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public Member create(MemberCreateDto memberCreateDto){
+        Member member = Member.builder()
+                .email(memberCreateDto.getEmail())
+                .password(passwordEncoder.encode(memberCreateDto.getPassword()))  //password  를 encoding 한 뒤 DB 에 저장
+                .build();
+        memberRepository.save(member);
+        return member;
+    }
+
+    public Member login(MemberLoginDto memberLoginDto){
+        Optional<Member> optMember = memberRepository.findByEmail(memberLoginDto.getEmail());
+        if(!optMember.isPresent()){
+            throw new IllegalArgumentException("email이 존재하지 않습니다.");
+        }
+
+        Member member = optMember.get();
+        if(!passwordEncoder.matches(memberLoginDto.getPassword(), member.getPassword())){
+            throw new IllegalArgumentException("password가 일치하지 않습니다.");
+        }
+        return member;
     }
 }
